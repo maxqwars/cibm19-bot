10d2cc85d20730493dcec607acc67ecd8a9f5edc
Maksim Maksimenko <maxqwars@gmail.com>
2024-06-03 09:26:10 +0700

    Extend Impact.ts

--- src/helpers/Impact.ts
+++ src/helpers/Impact.ts
@@ -1,1 +1,24 @@
-export default class Impact {}
+import { Context } from "telegraf";
+import { IBotCore } from "../modules/BotCore";
+import { Update } from "telegraf/typings/core/types/typegram";
+import logger from "../logger";
+
+const MOD_NAME = "[Impact]";
+
+type QueryCallbackType = {
+  (context: Context, core: IBotCore): Promise<void>;
+};
+
+type ImpactOptionsType = {
+  name: string;
+  signatureRegExp: RegExp;
+  callback: QueryCallbackType;
+};
+
+interface IImpactSonstructable {
+  new (options: ImpactOptionsType): IImpact;
+}
+
+interface IImpact {}
+
+export default class Impact implements IImpact {}
