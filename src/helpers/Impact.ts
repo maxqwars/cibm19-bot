14b2aac45006a0cea8d0d954162f000c2b9c4e49
Maksim Maksimenko <maxqwars@gmail.com>
2024-06-03 10:09:37 +0700

    Added getter for private props

--- src/helpers/Impact.ts
+++ src/helpers/Impact.ts
@@ -11,14 +11,40 @@ type QueryCallbackType = {
 
 type ImpactOptionsType = {
   name: string;
-  signatureRegExp: RegExp;
+  signature: RegExp;
   callback: QueryCallbackType;
 };
 
-export interface IImpactSonstructable {
+export interface IImpactConstructable {
   new (options: ImpactOptionsType): IImpact;
 }
 
-export interface IImpact {}
+export interface IImpact {
+  signature: RegExp;
+  name: string;
+  callback: QueryCallbackType;
+}
 
-export default class Impact implements IImpact {}
+export default class Impact implements IImpact {
+  private readonly _signature: RegExp;
+  private readonly _name: string;
+  private readonly _callback: QueryCallbackType;
+
+  constructor(options: ImpactOptionsType) {
+    this._callback = options.callback;
+    this._signature = options.signature;
+    this._name = options.name;
+  }
+
+  get signature() {
+    return this.signature;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get callback() {
+    return this._callback;
+  }
+}
