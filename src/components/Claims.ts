7946bd321074c6a1527ae06436faa19a7717e2dd
Maksim Maksimenko <maxqwars@gmail.com>
2024-06-06 11:26:08 +0700

    Added: Claims component

---
+++ src/components/Claims.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from "@prisma/client";
+import logger from "../logger";
+
+type ClaimCreateDataDto = {
+  volonteerId: number;
+  organizationId: number;
+};
+
+export class Claims {
+  private readonly _client: PrismaClient;
+
+  constructor(prisma: PrismaClient) {
+    this._client = prisma;
+  }
+
+  async create(dto: ClaimCreateDataDto) {
+    return await this._client.claim.create({
+      data: dto,
+    });
+  }
+
+  async volonteerClaims(volonteerId: number) {
+    return await this._client.claim.findMany({
+      where: {
+        volonteerId,
+      },
+    });
+  }
+
+  async claimDetails(id: number) {
+    return await this._client.claim.findFirst({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async organizationClaims(organizationId: number) {
+    return await this._client.claim.findMany({
+      where: {
+        organizationId,
+      },
+    });
+  }
+}
