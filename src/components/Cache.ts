8dce66c9b62293f74e760c876b51b8b4f9554ae7
Maksim Maksimenko <maxqwars@gmail.com>
2024-06-06 17:36:50 +0700

    Changed: Change logger messages

--- src/components/Cache.ts
+++ src/components/Cache.ts
@@ -11,7 +11,7 @@ export class Cache {
 
   async get(key: string): Promise<Buffer | null> {
     try {
-      this._log.info(`Getting cache value for key <${key}>`);
+      this._log.info(`[logger] Get cache value for key <${key}>...`);
       return new Promise((resolve, reject) => {
         this._client.get(key, (err, value) => {
           if (err) reject(err);
@@ -19,7 +19,9 @@ export class Cache {
         });
       });
     } catch (err) {
-      this._log.error(`Failed get cache value for key ${key}, reason:`);
+      this._log.error(
+        `[logger] Failed get cache value for key ${key}, reason:`,
+      );
       this._log.error(err.message);
       return null;
     }
@@ -32,7 +34,7 @@ export class Cache {
   ): Promise<Boolean | null> {
     try {
       this._log.info(
-        `Set cache value for key <${key}>, expires after ${expires} seconds`,
+        `[logger] Set cache value for key <${key}>, expires after ${expires} seconds`,
       );
       return new Promise((resolve, reject) => {
         this._client.set(key, value, { expires }, (err, success) => {
@@ -41,7 +43,9 @@ export class Cache {
         });
       });
     } catch (err) {
-      this._log.error(`Failed set cache value for key ${key}, reason:`);
+      this._log.error(
+        `[logger] Failed set cache value for key ${key}, reason:`,
+      );
       this._log.error(err.message);
       return null;
     }
